551b61381d0fc99b97df779876a351e2fdfcfb1c
J. Galkiewicz <46644880+j-galkiewicz@users.noreply.github.com>
2020-11-21 18:57:52 +0100

    js update

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function openSet() {
+    document.getElementById("set").classList.toggle("set-open"); //classlist.toggle -> adding a css element
+}
+
+document.getElementById("set-button").addEventListener('click', openSet);
+
+var userNickName = localStorage.getItem('receivedNickName');
+
+function saveNickName() {
+    localStorage.setItem('receivedNickName', userNickName); //1st argument is a keyword to get the info, 2nd argument - info that has to be rememeber 
+    if(userNickName == null) {
+        userNickName = "buddy";
+    }                                            
+}
+
+function changeNickName() {
+    userNickName = document.getElementById("nickname-input").value;
+    saveNickName;
+}
+
+document.getElementById("nickname-form").addEventListener('submit', function(e) {
+    e.preventDefault();
+    changeNickName;
+})
+
+function getHelloMessage() {
+    document.getElementById("hello").innerHTML = `Hello, ${userNickName}. In case you forgot - you rock!`;
+}
+
+getHelloMessage();
