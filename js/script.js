fa3f5cdbb0a735aef0a62df456b122ed648eb6ea
J. Galkiewicz <46644880+j-galkiewicz@users.noreply.github.com>
2020-11-22 13:54:24 +0100

    adjusting buttons & js code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
+
+//opening the settings field
 function openSet() {
     document.getElementById("set").classList.toggle("set-open"); //classlist.toggle -> adding a css element
 }
+//closing the settings field
+function closeSet() {
+    document.getElementById("set").classList.toggle("set-close"); 
+}
 
+
+document.getElementById("nickname-button").addEventListener('click', closeSet);
+
+//event listener that triggers the funciton whenever the settings button is clicked
 document.getElementById("set-button").addEventListener('click', openSet);
 
-var userNickName = localStorage.getItem('receivedNickName');
+
+//a variable to hold the name
+var userNickName;
 
 function saveNickName() {
     localStorage.setItem('receivedNickName', userNickName); //1st argument is a keyword to get the info, 2nd argument - info that has to be rememeber                                          
+    userNickName = localStorage.getItem('receivedNickName');
+    if(userNickName == null) {
+        userNickName = "buddy";
+    }  
 }
-
-if(userNickName == null) {
-    userNickName = "buddy";
-}  
   
 function changeNickName() {
-    userNickName = document.getElementById("nickname-input").value;
-    saveNickName;  
+    if(userNickName = document.getElementById("nickname-input").value == "") {
+        userNickName = "buddy";
+    } else {
+        userNickName = document.getElementById("nickname-input").value;
+    };
+    saveNickName();  
+    getHelloMessage();
+
+}
+
+function getHelloMessage() {
+    document.getElementById("hello").innerHTML = `Hello ${userNickName}. In case you forgot - you're a good boy 🐶`;
+    openSet();
 }
 
 
 document.getElementById("nickname-form").addEventListener('submit', function(e) {
     e.preventDefault();
-    changeNickName;
+    changeNickName();
 })
-
-function getHelloMessage() {
-    document.getElementById("hello").innerHTML = `Hello ${userNickName}. In case you forgot - keep smiling!`;
-}
-
-getHelloMessage();
